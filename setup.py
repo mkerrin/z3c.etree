bae392b16704c115194e4fa878a9f54503696610
mkerrin <mkerrin@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-02 17:55:13 +0000

    Add buildout packaging files.
    git-svn-id: svn://svn.zope.org/repos/main/z3c.etree/trunk@75019 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name = "z3c.etree",
+      version = "0.8",
+      author = "Michael Kerrin",
+      author_email = "michael.kerrin@openapp.ie",
+      url = "http://svn.zope.org/z3c.etree",
+      description = "Abstracts out the the implementation of elementtree " \
+                    "behind the zope component architecture",
+      license = "ZPL",
+
+      packages = find_packages("src"),
+      package_dir = {"": "src"},
+      install_requires = ["setuptools"],
+      extras_require = dict(test = ["zope.app.testing",
+                                    "elementtree",
+                                    "cElementTree",
+                                    "lxml"]),
+
+      include_package_data = True,
+      zip_safe = False)
