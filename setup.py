24ba2eb5d0351661106a932a0bc6fed7d865f820
mkerrin <mkerrin@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-07 19:01:49 +0000

    Bump version, add long description and add classifiers.
    git-svn-id: svn://svn.zope.org/repos/main/z3c.etree/trunk@83647 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(name = "z3c.etree",
-      version = "0.9.1",
+      version = "0.9.2",
       author = "Michael Kerrin",
       author_email = "michael.kerrin@openapp.ie",
       url = "http://svn.zope.org/z3c.etree",
-      description = "Abstracts out the the implementation of elementtree " \
-                    "behind the zope component architecture",
+      description = "Abstracts out the implementation of elementtree " \
+                    "behind the zope component architecture. And provides " \
+                    "helper functions for testing XML output in tests.",
+      long_description = (
+          open("src/z3c/etree/README.txt").read() +
+          "\n\n" +
+          open("CHANGES.txt").read()),
       license = "ZPL",
+      classifiers = ["Topic :: Software Development :: Testing",
+                     "Intended Audience :: Developers",
+                     "License :: OSI Approved :: Zope Public License",
+                     "Programming Language :: Python",
+                     "Framework :: Zope3",
+                     ],
 
       packages = find_packages("src"),
       package_dir = {"": "src"},
